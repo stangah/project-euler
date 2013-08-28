204845d2c3f987e96d981b2bbe4b4ad0561188d7
stangah <pstanger@gmail.com>
2013-08-28 11:34:25 -0400

    Added solution

--- question-016.js
+++ question-016.js
@@ -1,6 +1,38 @@
 /*
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 21000?
+What is the sum of the digits of the number 2^1000?
 */
 
+function eulerSixteen(power)
+{
+    var array = [1], r = 0, temp = 0, total = 0;
+    
+    // Create array for power of 2
+    for (var i = 1; i <= power; i++)
+	{
+		for (var j = 0; j < array.length; j++)
+		{
+			temp = array[j] * 2 + r;
+			r = 0;
+			if (temp >= 10)
+			{
+				r = Math.floor(temp / 10);
+				if (j === array.length - 1)
+				{
+					array.push(0);
+				}
+			}
+			array[j] = temp % 10;
+		}
+	}
+    
+    // Sum digits in array
+    for (var k = 0; k < array.length; k++)
+    {
+        total += array[k];
+    }
+	return total;
+}
+
+eulerSixteen(1000);
